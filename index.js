2d75a22a80c3978f9a6632e1fcec92cab090c14a
marifedt <57739727+marifedt@users.noreply.github.com>
2022-12-12 01:31:29 -0500

    Update on js file
    Wrap creation of boxes into a function
    Add event listeners for .box class on mouseenter and mouseleave
    Create changeColor function and generates random color for each call
    Add transition for the hover effect

--- index.js
+++ index.js
@@ -1,9 +1,30 @@
 const container = document.querySelector('.container');
 
-for (let i = 0; i < 16; i++) {
-    for (let j = 0; j < 16; j++) {
-        const square = document.createElement('div');
-        square.classList.add('box');
-        container.append(square);
+function createBoxes(){
+    for (let i = 0; i < 16; i++) {
+        for (let j = 0; j < 16; j++) {
+            const square = document.createElement('div');
+            square.classList.add('box');
+            container.append(square);
+        }
+    }
+
+    const boxes = document.querySelectorAll('.box');
+    boxes.forEach(box => box.addEventListener('mouseenter', changeColor));
+    boxes.forEach(box => box.addEventListener('mouseleave', changeColor));
+}
+
+function changeColor(e){
+    this.style.transition = `0.6s ease`;
+    if(e.type === 'mouseenter'){
+        let randomColor = Math.floor(Math.random()*16777215).toString(16);
+        this.style.backgroundColor = "#" + randomColor;
+    } else if(e.type === 'mouseleave'){
+        this.style.backgroundColor = '#FFFFFF';
     }
 }
+
+window.addEventListener('load', createBoxes);
+
+
+
