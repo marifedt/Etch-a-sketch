3470907d3f0d6d191f4e7cd87ecd92fedf78abe6
marifedt-w <57739727+marifedt@users.noreply.github.com>
2022-12-13 14:52:39 -0500

    Update approach on background color
    Change hex to rgb for background color
    Add two functions: randomNumber and randomRGBColor

--- index.js
+++ index.js
@@ -1,44 +1,52 @@
-const container = document.querySelector('.container');
-const btnChangeGrid = document.querySelector('button');
+const container = document.querySelector(".container");
+const btnChangeGrid = document.querySelector("button");
 
-function createBoxes(grid){
-    container.style.gridTemplateColumns = `repeat(${grid}, auto)`;
-
-    for (let i = 0; i < grid; i++) {
-        for (let j = 0; j < grid; j++) {
-            const square = document.createElement('div');
-            square.classList.add('box');
-            container.append(square);
-        }
-    }
-
-    const boxes = document.querySelectorAll('.box');
-    boxes.forEach(box => box.addEventListener('mouseenter', changeGridColor));
+function randomNumber(max) {
+  return Math.floor(Math.random() * (max + 1));
 }
 
-function changeGrid(){
-    let grid = prompt('Number of grids (Min:2 | Max:100)?');
-    if(Number.isInteger(parseInt(grid))){
-        if(!(grid < 2 || grid > 100)){
-            while (container.hasChildNodes()) {
-                container.removeChild(container.firstChild);
-        }
-        createBoxes(grid);
-        }  
-    }
+function randomRGBColor() {
+  let r = randomNumber(255);
+  let g = randomNumber(255);
+  let b = randomNumber(255);
+
+  return [r, g, b];
 }
 
-function changeGridColor(e){
-    this.style.transition = `0.2s ease`;
-    if(e.type === 'mouseenter'){
-        let maxColorVal = 0xFFFFFF;
-        let randomColor = Math.floor(Math.random()* maxColorVal).toString(16);
-        this.style.backgroundColor = "#" + randomColor;
+function createBoxes(grid) {
+  container.style.gridTemplateColumns = `repeat(${grid}, auto)`;
+
+  for (let i = 0; i < grid; i++) {
+    for (let j = 0; j < grid; j++) {
+      const square = document.createElement("div");
+      square.classList.add("box");
+      container.append(square);
     }
+  }
+
+  const boxes = document.querySelectorAll(".box");
+  boxes.forEach((box) => box.addEventListener("mouseenter", changeGridColor));
 }
 
-btnChangeGrid.addEventListener('click', changeGrid);
-window.addEventListener('load', createBoxes(64));
+function changeGrid() {
+  let grid = prompt("Number of grids (Min:2 | Max:100)?");
+  if (Number.isInteger(parseInt(grid))) {
+    if (!(grid < 2 || grid > 100)) {
+      while (container.hasChildNodes()) {
+        container.removeChild(container.firstChild);
+      }
+      createBoxes(grid);
+    }
+  }
+}
 
+function changeGridColor(e) {
+  this.style.transition = `0.2s ease`;
+  if (e.type === "mouseenter") {
+    const color = randomRGBColor();
+    this.style.backgroundColor = `rgb(${color[0]}, ${color[1]}, ${color[2]})`;
+  }
+}
 
-
+btnChangeGrid.addEventListener("click", changeGrid);
+window.addEventListener("load", createBoxes(64));
